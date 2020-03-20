48b1daea7c239a2dfdbde4b4061b637d1f32450a
harshithva <harshith11032001@gmail.com>
2020-03-20 12:20:23 +0530

    Working with components

--- my-app/src/App.vue
+++ my-app/src/App.vue
@@ -1,32 +1,20 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view />
+    <message>This is harshith</message>
+    <message></message>
+    <message></message>
+    <message></message>
   </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import Message from "./components/Message.vue";
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+export default {
+  name: "app",
+  components: { Message },
+  data() {
+    return {};
+  }
+};
+</script>

1b592586d048e8a86bb86c2c1e941b144b7fd1fd
harshithva <harshith11032001@gmail.com>
2020-03-20 11:06:35 +0530

    Named slots

--- js/main.js
+++ js/main.js
@@ -15,6 +15,30 @@ window.Event = new class {
 
 }
 
+Vue.component('modal-card', {
+  template: `
+  <div>
+  <div class="modal is-active">
+  <div class="modal-background"></div>
+  <div class="modal-card">
+    <header class="modal-card-head">
+      <p class="modal-card-title">
+      <slot name="title"></slot>
+      </p>
+      <button class="delete" aria-label="close"></button>
+    </header>
+    <section class="modal-card-body">
+    <slot></slot>
+    </section>
+    <footer class="modal-card-foot">
+      <button class="button is-success">Save changes</button>
+      <button class="button">Cancel</button>
+    </footer>
+  </div>
+</div>
+</div>`
+})
+
 Vue.component('coupon', {
   template: `<input type="text" placeholder="Enter coupon" @blur="onCouponApplied">`,
   methods: {
